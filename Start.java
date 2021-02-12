82396b37b87cb1369b8d5f8180b0421531eaec54
Saikiran-Vaddarapu <77972888+Saikiran-Vaddarapu@users.noreply.github.com>
2021-02-12 21:34:11 +0530

    Add files via upload

--- Start.java
+++ Start.java
@@ -4,47 +4,80 @@ public class Start extends A
 {
     public static void main(String args[]) throws Exception{
             new Start();
-            }
+            } 
        Start(){  
-               try{  
+               try{ 
        Class.forName("com.mysql.jdbc.Driver");
 
        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/first","root","root");
-       PreparedStatement r=null;        
+       PreparedStatement r=null;
+       PreparedStatement q=null;
+       PreparedStatement z=null;
+       Statement d=null;        
        Scanner s=new Scanner(System.in);
+       //int dg=5;
        //Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/first","root","root");
        System.out.println();
        System.out.println("***************************");
        System.out.println("Welcome to ABC Hotel");
        System.out.println("***************************");
-       System.out.println("Menu : 1.Register a room \n 2.Avail Services \n 3.Exiting the Hotel ");
+       System.out.println("Menu : 1.Register a room \n 2.Avail Services \n 3.Exciting the Hotel ");
        System.out.println("Enter the number against the options to proceed ");
        int t=s.nextInt();
        switch(t)
        {
            case 1: System.out.println("You are in a step closer to regiser the room ");
                    register();
-                   //System.out.println(ur);
-                   ur+=5;
-                   r=con.prepareStatement("insert into sample values(?,?,?)");
-                   r.setInt(1,ur);
+                   //System.out.println(ur)
+                   double y=Math.random() * 99;
+                   int x=(int)y;
+                   System.out.println("Following are your credentials for any support inside our premises");
+                   System.out.println("name : "+name);
+                   System.out.println("UniqueReference "+x);
+                   r=con.prepareStatement("insert into sample values(?,?,?,?)");
+                   r.setInt(1,x);
                    r.setString(2,name);
                    r.setString(3,name);
-                   System.out.println("Entering");
-                   r.executeUpdate();
+                   r.setInt(4,0);
+                   //System.out.println("Entering");
+                   int n=r.executeUpdate();
                    if(r==null)
                    {
                            System.out.println("Lol");
                    }
-                   con.close();
+                   //con.close();
+                   System.out.println(n);
                    //catch(Exception e){}
            //We need to call registration function here
                    break;
            case 2: System.out.println("Its glad that you wish to avail our services ");
            /* We need to call the services function here */
+                   System.out.println("Enter your ur");
+                   int c=s.nextInt();
+                   ///CONTINUE FROM HERE
+                   d=con.createStatement();
+                   ResultSet rs=d.executeQuery("select name from sample where roll = "+c);
+                   if(rs.next())
+                   System.out.println(rs.getString(1));
+                   service();
+                   //System.out.println(pr);
+                   q=con.prepareStatement("update sample set bill = ? where roll = ? ");
+                   q.setInt(1,pr);
+                   q.setInt(2,c);
+                   int i=q.executeUpdate();
+                   System.out.println(i);
                    break;
            case 3: System.out.println("Thanks for spending your time in our Hotel ");
            //We need to call Billing function here
+                   System.out.println("Enter your ur");
+                   int h=s.nextInt();
+                   ///CONTINUE FROM HERE
+                   d=con.createStatement();
+                   ResultSet ds=d.executeQuery("select name,bill from sample where roll = "+h);
+                   if(ds.next())
+                   System.out.println(ds.getString(1) +"  "+ ds.getInt(2));
+                   int pay=ds.getInt(2);
+                   System.out.println("Your bill is "+pay+" \n Pay the bill ");
                    break;
           default:System.out.println("Please Enter a correct option :) ");                       
        }}
@@ -57,7 +90,8 @@ class A{
         int days;
         int amt=1000;
         int[] a=new int[100];
-        static int i=0,ur=45;
+        static int i=0;
+        int pr=0;
            //Class.forName("com.mysql.jdbc.Driver");
            //Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/first","root","root");
            //Statement stmt=con.createStatement();
@@ -70,9 +104,9 @@ class A{
                 days=Sc.nextInt();
                 a[i]=1;
                 System.out.println("Your room is registered successfully...");
-                System.out.println("Following are your credentials for any support inside our premises");
+               /* System.out.println("Following are your credentials for any support inside our premises");
                 System.out.println("name : "+name);
-                System.out.println("UniqueReference "+ ur);
+                System.out.println("UniqueReference "); */
                 amt=amt*days;
                 System.out.println("Your rooms bill is "+amt);
                 System.out.println("Please pay this amount in advance as we dont charge for any other services till your exit.If paid enter 1 ");
@@ -81,7 +115,6 @@ class A{
                 if(check==1){
                 System.out.println("Yess,you can take your room,Dont forget to ping us for any services/support ");
                 i++;
-                ur++;
                 //r=con.prepareStatement("insert into sample values(ur,name,'BCoM')");
                 //r.executeUpdate();
                 //con.close();
@@ -92,152 +125,28 @@ class A{
                         //should implement
                  }
         } 
-      //catch(Exception e){}
-}
-        /* public static void main(String ar[]){
+      public void service(){
+              System.out.println("********************************* \n And Welcome to Services Department");
+              System.out.println("Menu of Services : \n 1.Doctor Consultation \n 2.Rent for a car \n"+ 
+              "3.Laundry and Cleaning \n 4.Misc ");
+              System.out.println("Enter number against wished Service ");
+              int ch=Sc.nextInt();
+              //int pr=0;
+              switch(ch)
+              {
+                case 1: System.out.println("Doctor is on the way :) ");
+                        pr+=100;
+                        break;
+                case 2: System.out.println("Your Cab is booked ");
+                        pr+=300;
+                        break;
+                case 3: System.out.println("Cleaning staff will be coming for cleaning");
+                        pr+=400;
+                        break;
+                case 4: System.out.println("Please call our staff for other services assistance ");
+                        break;
+                default: System.out.println("Please enter correct choice ");                        
+              }
+      }
+}        /* public static void main(String ar[]){
               new Start().regiser(); */
-import java.util.*;
-public class Main extends utils
-{
-    public static void main(String args[])
-    {
-            while(true)
-            {
-                new Main();
-            }
-    }
-            Main()
-       {
-        super();
-       System.out.println();
-       System.out.println("*********");
-       System.out.println("Welcome to ABC Hotel");
-       System.out.println("*********");
-       System.out.println("Menu : 1.Register a room \n 2.Avail Services \n 3.Exiting the Hotel ");
-       System.out.println("Enter the number against the options to proceed ");
-       int t=Sc.nextInt();
-       switch(t)
-         {
-           case 1: System.out.println("You are in a step closer to regiser the room ");
-                   register();
-           //We need to call registration function here
-                   break;
-           case 2: System.out.println("Its glad that you wish to avail our services ");
-           /* We need to call the services function here */
-                   Services();
-                   break;
-           case 3: System.out.println("Thanks for spending your time in our Hotel ");
-           //We need to call Billing function here
-                   billing();
-                   break;
-                   
-          default:System.out.println("Please Enter a correct option :) ");                      
-           }
-       }
-}  
-class utils{
-        int days;
-        static int amt=1000;
-        int [] a;
-        String name,VEG,NONVEG,order;
-        static int i=0,ur=5198;
-        static Scanner Sc=new Scanner(System.in);
-        utils(){
-        name = "";
-        days=0;
-        a=new int[100];
-        }
-        public void register(){
-                System.out.println("We are having sufficient number of rooms to facilitate Customers ");
-                System.out.print("  To proceed,we need certain details.\n Please enter your name : ");
-                Sc.nextLine();
-                name=Sc.nextLine();
-                System.out.print("Hello "+name+" how many days do you want to stay : ");
-                days=Sc.nextInt();
-                a[i]=1;
-                System.out.println("Your room is registered successfully...");
-                System.out.println("Following are your credentials for any support inside our premises");
-                System.out.println("name : "+name);
-                System.out.println("UniqueReference "+ ur);
-                amt=amt*days;
-                System.out.println("Your rooms bill is "+amt);
-                System.out.println("Please pay this amount in advance as we dont charge for any other services till your exit.If paid enter 1 ");
-                //String x=new String();
-                int check =Sc.nextInt();
-                if(check==1){
-                System.out.println("Yess,you can take your room,Dont forget to ping us for any services/support ");
-                i++;
-                ur+=7;
-                }
-                else
-                 {
-                        System.out.println("You are redirecting to main page");
-                        //should implement
-                 }
-        }
-        public void Services()
-        {
-            System.out.println("Please enter your unique reference code for services");
-          ur=Sc.nextInt();
-          System.out.println("Please choose what type of services you require from below");
-          System.out.println("Menu : 1.Cleaning \n 2.Food Services \n 3.Laundry \n 4.Medical Service \n 5.Booking a Cab \n ");
-            System.out.println("Thank you for your patience, we will get back to you in no time :) ");
-           int c=Sc.nextInt();
-           System.out.println("Thank you for calling, we will provide services in no time");
-           
-           
-           switch(c)
-           {
-            case 1: System.out.println("We will send the Cleaning team to your room \n");
-                   reception();
-            //We need to call reception here for cleaning service
-                   break;
-            case 2: System.out.println("Please state whether you prefer VEG or NONVEG,by entering v or n , it might take atleast 10 minutes for your order ");
-                    order=Sc.next();
-                    restaurant();
-        // We need to call the restaurant here
-                   break;
-            case 3: System.out.println("Our team will be there at your room shortly \n");
-           reception();
-            //We need to call reception here for laundry
-                   break;
-        case 4: System.out.println("Please tell your emergency doctor will be arriving");
-           reception();
-        //We need to call reception for Doctor or medical emergency
-           break;
-        case 5: System.out.println("Please state your destnation for cab services");
-           reception1();
-        //We need to call reception for cab
-           break;
-            default:System.out.println("Please Enter a correct option :) ");                      
-           }
-        }
-        public void reception()
-            {
-                amt=amt+300;
-             }
-    public void reception1()
-    {
-     System.out.println("Thank you for calling,be ready for cab");
-     amt=amt+500;
-    }
-    public void restaurant()
-   {
-    if(order.equals("v"))
-       {
-    System.out.println("Thank You for choosing Veg section, we will bring your order Fresh and Hot");
-    amt=amt+400;
-       }
-    else if(order.equals("n"))
-        {
-            System.out.println("Thank You for choosing NON-Veg section, we will bring your order Fresh and Hot");
-        amt=amt+500;
-        }
-    }
-    public void billing()
-        {
-          System.out.println("Hope you had a pleasant stay\nPlease pay the following amount");
-          System.out.println(amt+"\n.......Thank you.........");
-          System.exit(0);
-        }
- }
